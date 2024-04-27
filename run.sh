655aa0c9bf6834381cf04498942cffd5dd03ccd8
VinGP <105109338+VinGP@users.noreply.github.com>
2024-04-28 02:33:58 +0300

    add s3

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ source .env
 FILES="-f docker-compose.base.yml \
        -f ./backend/docker-compose.yml  \
        -f ./frontend/docker-compose.yml \
+       -f ./s3-worker/docker-compose.yml \
        -f docker-compose.yml"
 
 docker compose $FILES --env-file .env up --build 
